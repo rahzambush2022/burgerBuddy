df53ca3cd6b4b41e7b3c657a7c542659375c9e38
Taren Pang <tarenpang@gmail.com>
2022-05-03 12:13:30 -0700

    options for order form passed in using handlebars

--- models/Burger.js
+++ models/Burger.js
@@ -5,12 +5,6 @@ const burgerSchema = new Schema({
   bunType: String,
   pattyType: String,
   cheeseType: String,
-  toppings: [
-    { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'ToppingSet',
-    },
-  ],
 })
 
 module.exports = mongoose.model("Burger", burgerSchema);
